ffb7ede266b7d2901bc62dd3480f147b538d2f1f
Paweł Walecki <pawell.walecki@gmail.com>
2018-06-22 10:37:11 +0200

    Initial Commit

---
+++ Zadanie Rekrutacyjne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Zadanie Rekrutacyjne
+//
+//  Created by Paweł Walecki on 22.06.2018.
+//  Copyright © 2018 Paweł Walecki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Zadanie RekrutacyjneTests/Zadanie_RekrutacyjneTests.swift
@@ -0,0 +1,36 @@
+//
+//  Zadanie_RekrutacyjneTests.swift
+//  Zadanie RekrutacyjneTests
+//
+//  Created by Paweł Walecki on 22.06.2018.
+//  Copyright © 2018 Paweł Walecki. All rights reserved.
+//
+
+import XCTest
+@testable import Zadanie_Rekrutacyjne
+
+class Zadanie_RekrutacyjneTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
