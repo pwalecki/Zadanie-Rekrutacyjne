347c202e4b6b52e1494414b98042f1394403e3e6
Paweł Walecki <pawell.walecki@gmail.com>
2018-06-22 14:46:17 +0200

    add Element view cell and element, elements model

--- Zadanie Rekrutacyjne/Controller/Main View/MainViewController.swift
+++ Zadanie Rekrutacyjne/Controller/Main View/MainViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class MainViewController: BaseViewController {
+class MainViewController: BaseViewController, UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout {
+    
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var buttonView: UIView!
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopResetButton: UIButton!
+    
+    var elements = Elements()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,12 +24,46 @@ class MainViewController: BaseViewController {
         // Do any additional setup after loading the view.
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // Mark: - Buttons Action
+    
+    @IBAction func startTimerAction(_ sender: Any) {
+        elements.addNewElement()
+        collectionView.reloadData()
+    }
+    
 
+    @IBAction func stopResetTimerAction(_ sender: Any) {
+        elements.removeAllElements()
+        collectionView.reloadData()
+    }
+    
+    // Mark: - Collection View Methods
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return elements.arrayCount()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: ElementViewCell =  collectionView.dequeueReusableCell(withReuseIdentifier:    elementCellIdentifier , for: indexPath) as! ElementViewCell
+        
+            cell.setContentViewWithElement(element: elements.returnElemenetByIndex(index: indexPath.row))
+        
+        return cell;
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
